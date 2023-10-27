4aea3d8cfd30f60811ad97498cef92c1432c3835
Neelu Saraswatibhatla <neelu@sbneelu.com>
2023-10-27 11:36:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const URLS = {
+  example: "https://example.com",
+};
 
+function lookup(acronym) {
+  return acronym in URLS ? URLS[acronym] : undefined;
+}
+
+function main() {
+  const params = Object.fromEntries(new URLSearchParams(location.search));
+  const url = lookup(params.a);
+  if (url !== undefined) location.href = url;
+  else document.write("Invalid URL.");
+}
